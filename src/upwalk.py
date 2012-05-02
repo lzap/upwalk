25fcd4e9d97ce42ed75faec51cef5120ebba5f2a
Lukas Zapletal <lzap+git@redhat.com>
2012-05-02 22:50:02 +0200

    adding daemon code by Lone Wolves

--- src/upwalk.py
+++ src/upwalk.py
@@ -3,9 +3,10 @@ import datetime
 import logging
 import ConfigParser, os
 from os.path import abspath, dirname, join
+import sys, time
+from daemon import Daemon
 
 logging.basicConfig()
-
 config = ConfigParser.ConfigParser()
 config.readfp(open(join(abspath(dirname(__file__)), 'upwalk_default.conf')))
 config.read(['/etc/upwalk.conf', os.path.expanduser('~/.upwalk.conf')])
@@ -34,9 +35,32 @@ class SystemInfoJabberBot(JabberBot):
         """Tells you your username"""
         return mess.getFrom().getStripped()
  
-jid = config.get('connection', 'jid')
-password = config.get('connection', 'password')
-resource = config.get('connection', 'resource')
-debug = config.getboolean('connection', 'debug')
-bot = SystemInfoJabberBot(jid, password, resource, debug)
-bot.serve_forever()
+class UpwalkDaemon(Daemon):
+    def run(self):
+        jid = config.get('connection', 'jid')
+        password = config.get('connection', 'password')
+        resource = config.get('connection', 'resource')
+        debug = config.getboolean('connection', 'debug')
+        bot = SystemInfoJabberBot(jid, password, resource, debug)
+        bot.serve_forever()
+
+if __name__ == "__main__":
+    pid = config.get('daemon', 'pid')
+    log = config.get('daemon', 'log')
+    daemon = UpwalkDaemon(pid, stdout=log, stderr=log)
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        elif 'foreground' == sys.argv[1]:
+            daemon.run()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart|foreground" % sys.argv[0]
+        sys.exit(2)
